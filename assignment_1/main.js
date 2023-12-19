d3265f140dbe05d9997f1dac21ee8c7464a7553a
dziubenzo <dziubenzo@gmail.com>
2023-12-19 12:52:41 +0100

    Build iterative Fibonacci sequence function

--- assignment_1/main.js
+++ assignment_1/main.js
@@ -1,6 +1,22 @@
-const TEST_NUMBERS = [1, 2, 8, 30, 55];
+const TEST_NUMBERS = [0, 1, 2, 8, 19, 30, 55];
 
 // Iterative approach
 function fibs(number) {
-  
+  if (number === 0) {
+    return 0;
+  } else if (number === 1 || number === 2) {
+    return 1;
+  } else {
+    let result = 0;
+    let firstPrec = 1;
+    let secondPrec = 1;
+    for (let i = 3; i <= number; i++) {
+      result = firstPrec + secondPrec;
+      firstPrec = secondPrec;
+      secondPrec = result;
+    }
+    return result;
+  }
 }
+
+console.log(fibs(TEST_NUMBERS[6]));
