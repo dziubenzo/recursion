e8acfe81ce7f4d867b68b1790dc8328376d5c4d5
dziubenzo <dziubenzo@gmail.com>
2023-12-19 13:27:27 +0100

    Make sure an array of Fib seq values is returned

--- assignment_1/main.js
+++ assignment_1/main.js
@@ -2,21 +2,27 @@ const TEST_NUMBERS = [0, 1, 2, 8, 19, 30, 55];
 
 // Iterative approach
 function fibs(number) {
+  let array = [];
+  for (let i = 0; i <= number; i++) {
+    if (i === 0) {
+      array.push(0);
+    } else if (i === 1) {
+      array.push(1);
+    } else {
+      array.push(array[i - 2] + array[i - 1]);
+    }
+  }
+  return array;
+}
+
+// Recursive approach
+function fibsRec(number) {
   if (number === 0) {
     return 0;
   } else if (number === 1 || number === 2) {
     return 1;
   } else {
-    let result = 0;
-    let firstPrec = 1;
-    let secondPrec = 1;
-    for (let i = 3; i <= number; i++) {
-      result = firstPrec + secondPrec;
-      firstPrec = secondPrec;
-      secondPrec = result;
-    }
-    return result;
   }
 }
 
-console.log(fibs(TEST_NUMBERS[6]));
+console.log(fibs(TEST_NUMBERS[4]));
